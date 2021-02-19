39f7e503d6cb411604a11671eac80db4817206e1
Glitch (horse-materialistic-foxtail) <none>
2021-02-19 01:56:20 +0000

    💏💒 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -4,8 +4,46 @@ let imgList = [];
 let linkList = [];
 let timeList = [];
 
-
-
-for(let name of nameList) {
-  $(".nameDispaly").append(name)
+for (let img of imgList) {
+  $(".imgDisplay").append(img + ",");
 }
+
+for (let link of linkList) {
+  $(".linkDisplay").append(link + ",");
+}
+
+for (let time of timeList) {
+  $(".timeDisplay").append(time + ",");
+}
+
+$("button").click(function() {
+  let nameSmth = $(".nameInput").val();
+  let artistSmth = $(".artistInput").val();
+  let imgSmth = $(".imgInput").val();
+  let linkSmth = $(".linkInput").val();
+  let timeSmth = $(".timeInput").val();
+  nameList.push(nameSmth);
+  artistList.push(artistSmth);
+  imgList.push(imgSmth);
+  linkList.push(linkSmth);
+  timeList.push(timeSmth);
+  for (let name of nameList) {
+    $(".nameDisplay").append(name + ",");
+  }
+  for (let artist of artistList) {
+    $(".artistDisplay").append(artist + ",");
+  }
+  for (let img of imgList) {
+    $(".imgDisplay").append(img + ",");
+  }
+  for (let link of linkList) {
+    $(".linkDisplay").append(link + ",");
+  }
+  for (let time of timeList) {
+    $(".timeDisplay").append(time);
+  }
+});
+
+// use bob, bobby, bobert, bobara, and bobamantha to test everything out
+// and also this too https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Ffacebook%2F000%2F022%2F134%2Felmo.jpg
+// it's an elmo fire img

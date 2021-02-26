6bde707ea1567602a3fb89ef9c0889488060b79b
Glitch (horse-materialistic-foxtail) <none>
2021-02-26 01:53:42 +0000

    🍱👞 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -4,17 +4,17 @@ let imgList = [];
 let linkList = [];
 let timeList = [];
 
-for (let img of imgList) {
-  $(".imgDisplay").append(img + ",");
-}
+// for (let img of imgList) {
+//   $(".imgDisplay").append(img + ",");
+// }
 
-for (let link of linkList) {
-  $(".linkDisplay").append(link + ",");
-}
+// for (let link of linkList) {
+//   $(".linkDisplay").append(link + ",");
+// }
 
-for (let time of timeList) {
-  $(".timeDisplay").append(time + ",");
-}
+// for (let time of timeList) {
+//   $(".timeDisplay").append(time + ",");
+// }
 
 $("button").click(function() {
   let nameSmth = $(".nameInput").val();
@@ -28,22 +28,23 @@ $("button").click(function() {
   linkList.push(linkSmth);
   timeList.push(timeSmth);
   for (let name of nameList) {
-    $(".nameDisplay").append(name + ",");
+    $(".nameDisplay").append("<tr>" + "<td>" + name + "</td>");
   }
   for (let artist of artistList) {
-    $(".artistDisplay").append(artist + ",");
+    $(".artistDisplay").append("<td>" + artist + "</td");
   }
   for (let img of imgList) {
-    $(".imgDisplay").append(img + ",");
+    $(".imgDisplay").append();
   }
   for (let link of linkList) {
-    $(".linkDisplay").append(link + ",");
+    $(".linkDisplay").append();
   }
   for (let time of timeList) {
-    $(".timeDisplay").append(time);
+    $(".timeDisplay").append("<td>" + name + "</td>" + "</tr>");
   }
 });
 
 // use bob, bobby, bobert, bobara, and bobamantha to test everything out
 // and also this too https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Ffacebook%2F000%2F022%2F134%2Felmo.jpg
 // it's an elmo fire img
+// .reduce can be used as a time counter for the entire playlist to show how long it would take to finish it
